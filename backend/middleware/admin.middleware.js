db59e8fedf8fb95b5e2dd0463cb5986483374cf8
Balaj Maqbool <balajmaqbool54@gmail.com>
2026-01-12 19:13:30 +0500

    Remove backend routes and package.json files to streamline the project structure and eliminate unused code.

--- backend/middleware/admin.middleware.js
+++
@@ -1,8 +0,0 @@
-function admin(req, res, next) {
-  if (req.user.role !== 'admin') {
-    return res.status(403).send('Access denied! Admin privileges required.');
-  }
-  next();
-}
-
-module.exports = admin;
